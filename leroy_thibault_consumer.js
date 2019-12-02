a89410e6a7f5230a211258bced2b7cdbf0950b72
thibleroy <thibault.leroy.1@etudiant.com>
2019-12-02 15:40:47 +0100

    ok

--- leroy_thibault_consumer.js
+++ leroy_thibault_consumer.js
@@ -2,12 +2,12 @@ const kafka = require('kafka-node');
 const Consumer = kafka.Consumer;
 const client = new kafka.KafkaClient({kafkaHost: '148.60.11.202:9092'});
 const consumer = new Consumer(
-        client,
-        [
-            { topic: 'thibtopic', partitions: 1, offset: 5}
-        ],
-    { autoCommit: false, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024 * 1024 }
-    );
+    client,
+    [
+        { topic: this.topic, partitions: 1, fromOffset: -1}
+    ],
+    {autoCommit: false}
+);
 
 consumer.on('message', function (message) {
     console.log('message', message);
