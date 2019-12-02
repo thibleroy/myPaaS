d4cbf79e8ef1980d46fffdf79cf696b417861ee9
thibleroy <thibault.leroy.1@etudiant.com>
2019-12-02 16:12:10 +0100

    ok

--- leroy_thibault_consumer.js
+++ leroy_thibault_consumer.js
@@ -1,9 +1,23 @@
-const instances = require('./instances');
+const kafka = require('kafka-node');
+var consumerClient = new kafka.Client({kafkaHost: '148.60.11.202:9092'});
 
-instances.consumer.on('message', function (message) {
-    console.log('message', message);
-});
-instances.consumer.on('error', function (message) {
-    console.log('error', message);
+/* Print latest offset. */
+var offset = new kafka.Offset(consumerClient);
+
+offset.fetch([{ topic: 'thibtopic', partition: 0, time: -1 }], function (err, data) {
+    var latestOffset = data['myTopic']['0'][0];
+    console.log("Consumer current offset: " + latestOffset);
 });
 
+var consumer = new kafka.HighLevelConsumer(
+    consumerClient,
+    [
+        { topic: 'myTopic', partition: 0, fromOffset: -1 }
+    ],
+    {
+        autoCommit: false
+    }
+);
+consumer.on('message', (message) => {
+    console.log('message :', message);
+})
