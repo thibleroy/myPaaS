fe5c519878abe166e01503980dfd165b723ac4c5
thibleroy <thibault.leroy.1@etudiant.com>
2019-12-02 15:51:35 +0100

    ok

--- leroy_thibault_consumer.js
+++ leroy_thibault_consumer.js
@@ -1,17 +1,9 @@
-const kafka = require('kafka-node');
-const Consumer = kafka.Consumer;
-const client = new kafka.KafkaClient({kafkaHost: '148.60.11.202:9092'});
-const consumer = new Consumer(
-    client,
-    [
-        { topic: 'thibtopic', partitions: 1, fromOffset: -1}
-    ],
-    {autoCommit: false}
-);
+const instances = require('./instances');
 
-consumer.on('message', function (message) {
+instances.consumer.on('message', function (message) {
     console.log('message', message);
 });
-consumer.on('error', function (message) {
+instances.consumer.on('error', function (message) {
     console.log('error', message);
 });
+
