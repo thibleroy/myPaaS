61e4e6ba5e4486c7f57d3911469b48fe8cc108eb
thibleroy <thibault.leroy.1@etudiant.com>
2019-12-02 16:51:47 +0100

    ok

--- leroy_thibault_consumer.js
+++ leroy_thibault_consumer.js
@@ -1,5 +1,6 @@
 const instances = require('./instances');
 instances.consumer.on('message', (message) => {
+    console.log('lastoffset', instances.lastOffset);
     if (message.offset > instances.lastOffset) {
         console.log('message :', message);
     }
